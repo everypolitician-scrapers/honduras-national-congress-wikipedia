1aaaad28b3dd462d0ca56408916d61d16acfe129
Tony Bowden <tony@tmtm.com>
2018-07-19 12:37:07 +0100

    Migrate to Scraped

--- scraper.rb
+++ scraper.rb
@@ -1,39 +1,63 @@
 #!/bin/env ruby
-# encoding: utf-8
+# frozen_string_literal: true
 
-require 'scraperwiki'
-require 'nokogiri'
-require 'colorize'
 require 'pry'
+require 'scraped'
+require 'scraperwiki'
+require 'wikidata_ids_decorator'
+
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-class String
-  def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+class MembersPage < Scraped::HTML
+  decorator WikidataIdsDecorator::Links
+
+  field :members do
+    member_rows.map { |row| fragment(row => MemberRow).to_h }
+  end
+
+  private
+
+  def member_table
+    noko.xpath('//table[.//th[contains(.,"Diputado")]]')
+  end
+
+  def member_rows
+    member_table.xpath('.//tr[td]')
   end
 end
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
+class MemberRow < Scraped::HTML
+  field :id do
+    tds[0].css('a/@wikidata').map(&:text).first
+  end
 
-def scrape_list(url)
-  noko = noko_for(url)
-  noko.xpath('//table[.//caption[contains(.,"Diputados")]]//tr[td]').each do |tr|
-    tds = tr.css('td')
-    data = { 
-      name: tds[0].text.tidy,
-      wikiname: tds[0].xpath('.//a[not(@class="new")]/@title').text,
+  field :name do
+    tds[0].text.tidy
+  end
 
-      area: tds[1].text.tidy,
-      area_wikiname: tds[1].xpath('.//a[not(@class="new")]/@title').text,
+  field :area_id do
+    tds[1].css('a/@wikidata').map(&:text).first
+  end
 
-      party: tds[2].text.tidy,
-      party_wikiname: tds[2].xpath('.//a[not(@class="new")]/@title').text,
-    }
-    ScraperWiki.save_sqlite([:name, :area, :party], data)
+  field :area do
+    tds[1].css('a').map(&:text).first
+  end
+
+  field :party_id do
+    tds[2].css('a/@wikidata').map(&:text).last
+  end
+
+  field :party do
+    tds[2].css('a').map(&:text).last
+  end
+
+  private
+
+  def tds
+    noko.css('td')
   end
 end
 
-scrape_list('https://es.wikipedia.org/wiki/Anexo:Diputados_del_Congreso_Nacional_de_Honduras_2014-2018')
+url = 'https://es.wikipedia.org/wiki/Anexo:Diputados_del_Congreso_Nacional_de_Honduras_2014-2018'
+Scraped::Scraper.new(url => MembersPage).store(:members, index: %i[name area party])
